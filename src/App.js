a8264c621838de041f8e458944beed51cac5274f
mi66mc <miguelmalgarezi@gmail.com>
2022-11-27 20:42:18 -0300

    Themes and bugs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
 
+export const ThemeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   const [input, setInput] = useState();
+
   return (
-    <div className="App">
-      <textarea
-        className="textarea"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        spellcheck="false"
-        placeholder="Markdown code..."/>
-      <ReactMarkdown children={input} className="markdown" remarkPlugins={[remarkGfm]}/>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <textarea
+          className="textarea"
+          id={theme}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          spellcheck="false"
+          placeholder="Markdown code..."/>
+        <div className="mark" id={theme}>
+          <ReactMarkdown children={input} className="markdown" remarkPlugins={[remarkGfm]}/>
+        </div>
+        <div className="themeBtn">
+          <button onClick={toggleTheme} id={theme}>{theme === "light" ? "Light Mode" : "Dark Mode"}</button>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
